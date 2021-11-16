f0a969b66eb0302a6139a312337d9a517fab9146
Godra Adam <g.adam99@freemail.hu>
2021-11-17 00:25:28 +0200

    fix lint errors

--- src/main/java/dev/godraadam/dsassingment/api/controller/MeasurementController.java
+++ src/main/java/dev/godraadam/dsassingment/api/controller/MeasurementController.java
@@ -1,13 +1,9 @@
 package dev.godraadam.dsassingment.api.controller;
 
-import java.time.LocalDate;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.godraadam.dsassingment.api.dto.MeasurementDTO;
 
 @RestController
 @CrossOrigin
--- src/main/java/dev/godraadam/dsassingment/service/MeasurementService.java
+++ src/main/java/dev/godraadam/dsassingment/service/MeasurementService.java
@@ -1,18 +1,12 @@
 package dev.godraadam.dsassingment.service;
 
-import java.time.LocalDate;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import dev.godraadam.dsassingment.model.Measurement;
-import dev.godraadam.dsassingment.repo.MeasurementRepo;
 
 @Service
 public class MeasurementService {
 
-    @Autowired
-    private MeasurementRepo measurementRepo;
 
 
 }
