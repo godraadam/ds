0566158d30bf18f3cb6aa34bdfdfba211d54fada
Godra Adam <g.adam99@freemail.hu>
2021-11-17 12:24:26 +0200

    updated sql import with address fields

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 -- bootstrap initial admin account
-insert into app_user (id, email, password, role, first_name, last_name) values (0, 'g.adam@throwaway.com', '$2a$12$.tyUWmptZ1IXUPNUEsO3jO9F/u5D0LKdMWq6KM796DOifqHQcE33.', 'ADMIN', 'Godra', 'Adam');
-insert into app_user (id, email, password, role, first_name, last_name) values (1, 'g.adam2@throwaway.com', '$2a$12$.tyUWmptZ1IXUPNUEsO3jO9F/u5D0LKdMWq6KM796DOifqHQcE33.', 'USER', 'Godra', 'Adam');
+insert into app_user (id, email, password, role, first_name, last_name, address) values (0, 'g.adam@throwaway.com', '$2a$12$.tyUWmptZ1IXUPNUEsO3jO9F/u5D0LKdMWq6KM796DOifqHQcE33.', 'ADMIN', 'Godra', 'Adam', 'Cluj-Napoca Str. Gh. Baritiu nr 1');
+insert into app_user (id, email, password, role, first_name, last_name, address) values (1, 'g.adam2@throwaway.com', '$2a$12$.tyUWmptZ1IXUPNUEsO3jO9F/u5D0LKdMWq6KM796DOifqHQcE33.', 'USER', 'Godra', 'Adam the II', 'Cluj-Napoca Str. Gh. Baritiu nr 1');
 insert into device (id, address, avg_consumption, max_consumption, description, user_id) values(0, 'Example address', 0, 12000, 'Example device', 0)
