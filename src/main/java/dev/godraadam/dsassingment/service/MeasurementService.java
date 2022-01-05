c2cb79f814a8b3754be054152ec19f36bc29bf33
godraadam <godra.adam@gmail.com>
2022-01-05 14:33:07 +0200

    fix lint issue

--- src/main/java/dev/godraadam/dsassingment/service/MeasurementService.java
+++ src/main/java/dev/godraadam/dsassingment/service/MeasurementService.java
@@ -8,8 +8,6 @@ import java.util.List;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
